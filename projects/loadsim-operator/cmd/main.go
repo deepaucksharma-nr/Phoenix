f9190a3b93d20f1668e38499a6ec9a3bde6f1450
deepaucksharma-nr <deepaksharma@newrelic.com>
2025-05-26 13:25:15 +0530

    feat: Complete loadsim-operator CRD implementation
    - Added LoadSimulationJob CRD types
    - Implemented controller reconciliation logic
    - Added load generator implementation
    - Ready for Kubernetes operator deployment

--- projects/loadsim-operator/cmd/main.go
+++ projects/loadsim-operator/cmd/main.go
@@ -1,31 +1,95 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"go.uber.org/zap"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	phoenixv1alpha1 "github.com/phoenix-vnext/platform/projects/loadsim-operator/api/v1alpha1"
+	"github.com/phoenix-vnext/platform/projects/loadsim-operator/internal/controller"
 )
 
+var (
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(phoenixv1alpha1.AddToScheme(scheme))
+}
+
 func main() {
-	// Initialize logger
-	logger, err := zap.NewProduction()
-	if err != nil {
-		panic(fmt.Sprintf("failed to initialize logger: %v", err))
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+
+	opts := zap.Options{
+		Development: true,
 	}
-	defer logger.Sync()
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
 
-	logger.Info("starting loadsim operator",
-		zap.String("version", "0.1.0"),
-		zap.String("status", "not-implemented"),
-	)
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-	// TODO: Implement loadsim operator
-	// 1. Set up controller-runtime manager
-	// 2. Register LoadSimulationJob controller
-	// 3. Create/manage simulator Jobs
-	// 4. Clean up completed jobs
+	// Initialize production logger for application use
+	prodLogger, err := zap.NewProduction()
+	if err != nil {
+		setupLog.Error(err, "unable to create zap logger")
+		os.Exit(1)
+	}
+	defer prodLogger.Sync()
 
-	logger.Warn("loadsim operator not yet implemented")
-	os.Exit(0)
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "loadsim.phoenix.io",
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
+
+	// Setup LoadSimulationJob controller
+	if err = (&controller.LoadSimulationJobReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+		Logger: prodLogger,
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LoadSimulationJob")
+		os.Exit(1)
+	}
+
+	// Setup health checks
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
+
+	setupLog.Info("starting loadsim operator", "version", "0.1.0")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
